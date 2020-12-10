869613bdb5e9b1f869fe89382316f11a7a7c4352
trifon.yanakiev@gmail.com <trifon.yanakiev@gmail.com>
2020-12-09 22:54:37 -0600

    Pulling relationship data from Ratings.

--- MovieRecommend/movieviewer/views.py
+++ MovieRecommend/movieviewer/views.py
@@ -11,20 +11,20 @@ def index(request):
         form = MoviesSearch(request.POST)
         if form.is_valid():
             if form.cleaned_data['title']:
-                movies_data = Movie.objects.all().filter(primaryTitle__icontains=form.cleaned_data['title'],
-                                                         titleType='movie')
+                movies_data = Movie.objects.all().filter(rating__isnull=False, primaryTitle__icontains=form.cleaned_data['title'],
+                                                         titleType='movie', ).order_by("-rating__averageRating")
             else:
-                movies_data = Movie.objects.all().filter(titleType='movie')
+                movies_data = Movie.objects.all().filter(rating__isnull=False, titleType='movie').order_by("-rating__averageRating")
 
-            paginator = Paginator(movies_data, 28)
+            paginator = Paginator(movies_data, 52)
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
             return render(request, 'movieviewer/index.html', {'movies': page_obj, 'form': form})
 
     else:
         form = MoviesSearch()
-        movies_data = Movie.objects.all().filter(titleType='movie')
-        paginator = Paginator(movies_data, 28)
+        movies_data = Movie.objects.all().filter(rating__isnull=False, titleType='movie').order_by("-rating__averageRating")
+        paginator = Paginator(movies_data, 52)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, 'movieviewer/index.html', {'movies': page_obj, 'form': form})
